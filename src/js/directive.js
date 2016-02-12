105e574b0496b55c84db05540da95950292fda1c
pmill <dev.pmill@gmail.com>
2016-02-12 20:52:24 +0000

    Updated example to show how to use ng-repeat and toggle event Added id to directive scope Added styles

--- src/js/directive.js
+++ src/js/directive.js
@@ -4,15 +4,25 @@
   angular.module('angular-toggle', []).directive('pmToggle', function() {
     return {
       restrict: 'E',
-      template: '<p>Hello</p>',
+      scope: {
+        id: '=',
+        ngModel: '='
+      },
+      template: '<div class="pm-toggle"><label><input type="checkbox" ng-model="vm.ngModel" ng-change="vm.changed()"><strong></strong></label></div>',
       replace: true,
       controller: ToggleController,
       controllerAs: 'vm',
       bindToController: true
     };
-
-    function ToggleController() {
-      var vm = this;
-    }
   });
+
+  ToggleController.$inject = ['$scope'];
+
+  function ToggleController($scope) {
+    var vm = this;
+
+    vm.changed = function () {
+      $scope.$emit('pm-toggle.changed', { id: vm.id, enabled: vm.ngModel});
+    };
+  }
 })();
